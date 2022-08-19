3658afebe105811ab29f0265ed6877ce4bed9071
AdelaideAA <arvatiadelaideyahoo.fr>
2022-08-19 11:38:28 +0200

    comment gestion image du delete

--- back/controllers/publication.js
+++ back/controllers/publication.js
@@ -95,12 +95,12 @@ exports.deletePost = (req, res, next) => {
 			if (post.userId != req.auth.userId) {
 				res.status(401).json({ message: 'Non-autorisé' })
 			} else {
-				const filename = post.imageUrl.split('images/')[1];
-				fs.unlink(`images/${filename}`, () => {
+				// const filename = post.imageUrl.split('images/')[1];
+				// fs.unlink(`images/${filename}`, () => {
 				Post.deleteOne({ _id: req.params.id })
 					.then(() => res.status(200).json({ message: 'Post supprimé !' }))
 					.catch((error) => res.status(401).json({ error }))
-				});
+				//});
 			}
 		})
 		.catch((error) => res.status(500).json({ error }))
